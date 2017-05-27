f89cd0c691fe9bcc2e58015e624e54e97e86ba62
JosepRivaille <josep.de.cid@gmail.com>
2017-05-27 17:20:36 +0200

    Add test suites for basic version and script to execute it

--- executable.py
+++ executable.py
@@ -1,19 +1,32 @@
 import platform
-from sys import stdin, argv
+import argparse
 import os
-
+from os.path import isfile, join
 
 if __name__ == '__main__':
-	version = argv[1]
+	parser = argparse.ArgumentParser(description='Executes PDDL RicoRico versions')
+	parser.add_argument('version', metavar='version', type=int, help='Version number')
+	parser.add_argument('--test', dest='test', action='store_const', const=True, default=False, help='Execute test suites')
 
-	file_domain = os.path.join('versions', 'v' + version, 'rico_rico_domain.pddl')
-	file_problem = os.path.join('versions', 'v' + version, 'rico_rico.pddl')
-	ff_args = ' -o ' + file_domain + ' -f ' + file_problem
-	
+	args = parser.parse_args()
+	version = str(args.version)
+	executeTests = args.test
+
 	available_executables = {
-		'Windows': os.path.join('windows', 'ff.exe') + ff_args,
-		'Linux': os.path.join('linux', 'ff') + ff_args
+		'Windows': os.path.join('windows', 'ff.exe'),
+		'Linux': os.path.join('linux', 'ff')
 	}
 	current_os = platform.system()
 
-	os.system(available_executables.get(current_os, "echo 'Error!'"))
+	file_domain = os.path.join('versions', 'v' + version, 'rico_rico_domain.pddl')
+
+	if not executeTests:
+		file_problem = os.path.join('versions', 'v' + version, 'rico_rico.pddl')
+		ff_args = ' -o ' + file_domain + ' -f ' + file_problem
+		os.system(available_executables.get(current_os) + ff_args)
+	else:
+		tests = instance_files = [f for f in os.listdir(os.path.join('versions', 'v' + version)) if '_ts_' in f]
+		for test in tests:
+			ff_args = ' -o ' + file_domain + ' -f ' + os.path.join('versions', 'v' + version, test)
+			print('//// EXECUTING TEST -> ' + test + ' ////')
+			os.system(available_executables.get(current_os) + ff_args)
