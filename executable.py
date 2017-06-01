1a2c673e4245e4ae42cbf46e40fd811b4af61799
Josep de Cid <josep.de.cid@gmail.com>
2017-06-01 15:53:57 +0200

    Fix python scripts

--- executable.py
+++ executable.py
@@ -1,32 +1,42 @@
-import platform
 import argparse
 import os
-from os.path import isfile, join
+import platform
+import sys
+
+
+def print_error():
+    print('Your OS is not currently supported!')
+    sys.exit(1)
+
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Executes PDDL RicoRico versions')
-	parser.add_argument('version', metavar='version', type=int, help='Version number')
-	parser.add_argument('--test', dest='test', action='store_const', const=True, default=False, help='Execute test suites')
+    parser = argparse.ArgumentParser(description='Executes PDDL RicoRico versions')
+    parser.add_argument('version', metavar='version', type=int, help='Version number')
+    parser.add_argument('--test', dest='test', action='store_const', const=True, default=False,
+                        help='Execute test suites')
 
-	args = parser.parse_args()
-	version = str(args.version)
-	executeTests = args.test
+    args = parser.parse_args()
+    version = str(args.version)
+    executeTests = args.test
 
-	available_executables = {
-		'Windows': os.path.join('windows', 'ff.exe'),
-		'Linux': os.path.join('linux', 'ff')
-	}
-	current_os = platform.system()
+    available_executables = {
+        'Windows': os.path.join('windows', 'ff.exe'),
+        'Linux': os.path.join('linux', 'ff')
+    }
+    current_os = platform.system()
 
-	file_domain = os.path.join('versions', 'v' + version, 'rico_rico_domain.pddl')
+    file_domain = os.path.join('versions', 'v' + version, 'rico_rico_domain.pddl')
+    executable = available_executables.get(current_os, None)
+    if executable is None:
+        print_error()
 
-	if not executeTests:
-		file_problem = os.path.join('versions', 'v' + version, 'rico_rico.pddl')
-		ff_args = ' -o ' + file_domain + ' -f ' + file_problem
-		os.system(available_executables.get(current_os) + ff_args)
-	else:
-		tests = instance_files = [f for f in os.listdir(os.path.join('versions', 'v' + version)) if '_ts_' in f]
-		for test in tests:
-			ff_args = ' -o ' + file_domain + ' -f ' + os.path.join('versions', 'v' + version, test)
-			print('//// EXECUTING TEST -> ' + test + ' ////')
-			os.system(available_executables.get(current_os) + ff_args)
+    if not executeTests:
+        file_problem = os.path.join('versions', 'v' + version, 'rico_rico.pddl')
+        ff_args = ' -o ' + file_domain + ' -f ' + file_problem
+        os.system(executable + ff_args)
+    else:
+        tests = instance_files = [f for f in os.listdir(os.path.join('versions', 'v' + version)) if '_ts_' in f]
+        for test in tests:
+            ff_args = ' -o ' + file_domain + ' -f ' + os.path.join('versions', 'v' + version, test)
+            print('//// EXECUTING TEST -> ' + test + ' ////')
+            os.system(executable + ff_args)
--- generator.py
+++ generator.py
@@ -1,104 +1,122 @@
-import platform
 import argparse
 import os
 import random
-from os.path import isfile, join
 
-def generate_predicate(pred):
-  args = pred['args']
-  predicate = '(' + pred['name']
-  for arg in range(args):
-    predicate += ' ' + random.choice(pred['types'][arg]['values'])
-  return predicate + ')'
+
+def generate_predicate(genpred):
+    arguments = genpred['args']
+    predicate = '(' + genpred['name']
+    for arg in range(arguments):
+        predicate += ' ' + random.choice(genpred['types'][arg]['values'])
+    return predicate + ')'
+
 
 def generate_day_before(days):
-  day_before = ''
-  for i in range(len(days) - 1):
-    day_before += 4*' ' + '(dayBefore ' + days[i] + ' ' + days[i+1] + ')\n'
-  return day_before + '\n'
+    day_before = ''
+    for i in range(len(days) - 1):
+        day_before += 4 * ' ' + '(dayBefore ' + days[i] + ' ' + days[i + 1] + ')\n'
+    return day_before + '\n'
+
+
+def generate_classifications(main_courses, second_courses, categories):
+    categories_mc = [5, 3, 6, 4, 6, 0, 1]
+    categories_sc = [1, 1, 4, 0, 1, 1, 6]
+
+    categorization = ''
+    for index, course in enumerate(main_courses):
+        categorization += 4 * ' ' + '(classified ' + course + ' ' + categories[categories_mc[index]] + ')\n'
+    for index, course in enumerate(second_courses):
+        categorization += 4 * ' ' + '(classified ' + course + ' ' + categories[categories_sc[index]] + ')\n'
+    return categorization + '\n'
+
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description='Executes PDDL RicoRico versions')
-  parser.add_argument('version', metavar='version', type=int, help='Version number')
-  parser.add_argument('tests', metavar='tests', type=int, help='Number of tests')
+    parser = argparse.ArgumentParser(description='Executes PDDL RicoRico versions')
+    parser.add_argument('version', metavar='version', type=int, help='Version number')
+    parser.add_argument('tests', metavar='tests', type=int, help='Number of tests')
 
-  args = parser.parse_args()
-  version = args.version
-  n_tests = args.tests
+    args = parser.parse_args()
+    version = args.version
+    n_tests = args.tests
 
-  file_tpl = os.path.join('templates', 'problem.tpl')
-  data = {
-    'objects': [],
-    'init': [],
-    'goal': []
-  }
-
-  # Objects
-  day = {
-    'values': ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-    'type': 'day'
-  }
-  data['objects'].append(day)
-  
-  main_course = {
-    'values': ['Spaghetti_Bolognese', 'Mediterranean_Salad', 'Vegan_Sandwich', 'Mushroom_risotto', 'Guacamole_with_tomatoes', 'Sushi', 'American_burger'],
-    'type': 'mainCourse'
-  }
-  data['objects'].append(main_course)
-  
-  second_course = {
-    'values': ['Roast_pork_with_prunes', 'Spanish_omelette', 'Paella', 'Tuna_steak', 'Chicken_parmesan', 'Lamb_tagine', 'Couscous_meatloaf'],
-    'type': 'secondCourse'
-  }
-  data['objects'].append(second_course)
-  
-  dish = {
-    'values': main_course['values'] + second_course['values'],
-    'type': 'dish'
-  }
-
-  # Init
-  data['init'].append({'name': 'incompatible', 'random': True, 'args': 2, 'types': [main_course, second_course]})
-
-  if version >= 1:
-    pass
-
-  if version >= 2:
-    category = {
-      'values': ['Fish', 'Meat', 'Soup', 'Salad', 'Rice', 'Pasta', 'Vegetables'],
-      'type': 'category'
+    file_tpl = os.path.join('templates', 'problem.tpl')
+    data = {
+        'objects': [],
+        'init': [],
+        'goal': []
     }
-    data['objects'].append(category)
-    
-    data['init'].append({'name': 'classified', 'random': True, 'args': 2, 'types': [dish, category]})
-    data['init'].append({'name': 'dayBefore', 'random': False, 'values': generate_day_before(day['values'])})
 
-  if version >= 3:
-    data['init'].append({'name': 'servedOnly', 'random': True, 'args': 2, 'types': [dish, day]})
+    # Objects
+    day = {
+        'values': ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+        'type': 'day'
+    }
+    data['objects'].append(day)
 
-  # Create objects
-  objects = ''
-  for obj in data['objects']:
-    objects += 4*' ' + ' '.join(obj['values']) + ' - ' + obj['type'] + '\n'
-  objects = objects[:-1]
+    main_course = {
+        'values': ['Spaghetti_Bolognese', 'Mediterranean_Salad', 'Vegan_Sandwich', 'Mushroom_risotto',
+                   'Guacamole_with_tomatoes', 'Sushi', 'American_burger'],
+        'type': 'mainCourse'
+    }
+    data['objects'].append(main_course)
 
-  # Create init
-  init = ''
-  for pred in data['init']:
-    if pred['random']:
-      for _ in range(random.randint(1, 5)):
-        init += 4*' ' + generate_predicate(pred) + '\n'
-      init += '\n'
-    else:
-      init += pred['values']
-  init = init[:-2]
+    second_course = {
+        'values': ['Roast_pork_with_prunes', 'Spanish_omelette', 'Paella', 'Tuna_steak', 'Chicken_parmesan',
+                   'Lamb_tagine', 'Couscous_meatloaf'],
+        'type': 'secondCourse'
+    }
+    data['objects'].append(second_course)
 
-  # Create goal
-  goal = 4*' ' + '(forall (?d - day)\n' + 6*' ' + '(dayReady ?d)\n' + 4*' ' + ')'
-  
+    dish = {
+        'values': main_course['values'] + second_course['values'],
+        'type': 'dish'
+    }
 
-  for n in range(n_tests):
-    with open(file_tpl, 'r') as f:
-      template = f.read()
-      template = template.format(problem='ricoRico', domain='ricoRico', objects=objects, init=init, goal=goal)
-      print(template)
+    # Init
+    data['init'].append({'name': 'incompatible', 'random': True, 'args': 2, 'types': [main_course, second_course]})
+
+    if version >= 1:
+        pass
+
+    if version >= 2:
+        category = {
+            'values': ['Fish', 'Meat', 'Soup', 'Salad', 'Rice', 'Pasta', 'Vegetables'],
+            'type': 'category'
+        }
+        data['objects'].append(category)
+
+        data['init'].append({'name': 'classified', 'random': False, 'values': generate_day_before(day['values'])})
+        data['init'].append({'name': 'dayBefore', 'random': False, 'values': generate_classifications(
+            main_course['values'],
+            second_course['values'],
+            category['values']
+        )})
+
+    if version >= 3:
+        data['init'].append({'name': 'servedOnly', 'random': True, 'args': 2, 'types': [dish, day]})
+
+    # Create objects
+    objects = ''
+    for obj in data['objects']:
+        objects += 4 * ' ' + ' '.join(obj['values']) + ' - ' + obj['type'] + '\n'
+    objects = objects[:-1]
+
+    # Create init
+    init = ''
+    for pred in data['init']:
+        if pred['random']:
+            for _ in range(random.randint(1, 5)):
+                init += 4 * ' ' + generate_predicate(pred) + '\n'
+            init += '\n'
+        else:
+            init += pred['values']
+    init = init[:-2]
+
+    # Create goal
+    goal = 4 * ' ' + '(forall (?d - day)\n' + 6 * ' ' + '(dayReady ?d)\n' + 4 * ' ' + ')'
+
+    for n in range(n_tests):
+        with open(file_tpl, 'r') as f:
+            template = f.read()
+            template = template.format(problem='ricoRico', domain='ricoRico', objects=objects, init=init, goal=goal)
+            print(template)
